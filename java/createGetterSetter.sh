7fb46073a21cd66bd5318fedd698b831ec533180
Adam Huang <poisondog@gmail.com>
2021-03-26 14:13:03 +0800

    move groupTogether.py to tool folder

--- java/createGetterSetter.sh
+++ java/createGetterSetter.sh
@@ -2,5 +2,5 @@
 
 sed -i 's/\(\s*private\s*\(\S*\)\s*\(\S*\);\)/\1\npublic \2 get\u\3() {\nreturn \3;\n}\npublic void set\u\3(\2 \3) {\nthis.\3 = \3;\n}\n/g' $1
 
-python3 groupTogether.py $1 '^\s*private\s*\S*\s*\S*\s*;\s*$'
+python3 tool/groupTogether.py $1 '^\s*private\s*\S*\s*\S*\s*;\s*$'
 
