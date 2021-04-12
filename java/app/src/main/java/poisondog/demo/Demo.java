48974d73d75e07e0a3bff2460d0eb44eecb06704
Adam Huang <poisondog@gmail.com>
2021-04-12 14:11:06 +0800

    add getMethods in java app

--- java/app/src/main/java/poisondog/demo/Demo.java
+++ java/app/src/main/java/poisondog/demo/Demo.java
@@ -15,6 +15,12 @@
  */
 package poisondog.demo;
 
+import java.lang.Class;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Adam Huang <poisondog@gmail.com>
  */
@@ -24,12 +30,44 @@ public class Demo {
 		builder.append(getClass().getName());
 		return builder.toString();
 	}
+	private static List<String> getMethods(Class clz) {
+		List<String> publics = new ArrayList<String>();
+		List<String> protecteds = new ArrayList<String>();
+		List<String> privates = new ArrayList<String>();
+		while (clz != null) {
+			for (Method method : clz.getDeclaredMethods()) {
+				int modifiers = method.getModifiers();
+				if (Modifier.isPrivate(modifiers)) {
+					privates.add(method.toString());
+				}
+				if (Modifier.isProtected(modifiers)) {
+					protecteds.add(method.toString());
+				}
+				if (Modifier.isPublic(modifiers)) {
+					publics.add(method.toString());
+				}
+			}
+			clz = clz.getSuperclass();
+		}
+		List<String> result = new ArrayList<String>();
+		result.addAll(privates);
+		result.addAll(protecteds);
+		result.addAll(publics);
+		return result;
+	}
+
 	public static void main (String [] args){
+		System.out.println(System.currentTimeMillis());
 		Demo de = new Demo();
 		System.out.println("getName(): " + Demo.class.toString());
 		System.out.println(new Hello());
 		for (int in = 0; in < 100; in++) {
 			System.out.println("08:" + (int)(Math.random() * 25 + 30) + " → " + String.format("18:%02d", (int)(Math.random() * 12)));
 		}
+		System.out.println("1,00abj20hw".replaceAll("\\D", ""));
+		for (String line : getMethods(Demo.class)) {
+			System.out.println(line);
+		}
+
 	}
 }
